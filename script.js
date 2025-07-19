18fa3e430e659e5dca507c03144bac0b19aeae38
Johnson Topno <johnsontopno1@gmail.com>
2025-07-19 18:39:20 +0530

    upgrade
    upgrade

--- script.js
+++ script.js
@@ -1,63 +1,60 @@
 $(function () {
-    $(".sidebar-link").click(function () {
-     $(".sidebar-link").removeClass("is-active");
-     $(this).addClass("is-active");
-    });
-   });
-   
-   $(window)
-    .resize(function () {
-     if ($(window).width() > 1090) {
+  $(".sidebar-link").click(function () {
+    $(".sidebar-link").removeClass("is-active");
+    $(this).addClass("is-active");
+  });
+});
+
+$(window)
+  .resize(function () {
+    if ($(window).width() > 1090) {
       $(".sidebar").removeClass("collapse");
-     } else {
+    } else {
       $(".sidebar").addClass("collapse");
-     }
-    })
-    .resize();
-   
-   const allVideos = document.querySelectorAll(".video");
-   
-   allVideos.forEach((v) => {
-    v.addEventListener("mouseover", () => {
-     const video = v.querySelector("video");
-     video.play();
-    });
-    v.addEventListener("mouseleave", () => {
-     const video = v.querySelector("video");
-     video.pause();
-    });
-   });
-   
-   $(function () {
-    $(".logo, .logo-expand, .discover").on("click", function (e) {
-     $(".main-container").removeClass("show");
-     $(".main-container").scrollTop(0);
-    });
-    $(".trending, .video").on("click", function (e) {
-     $(".main-container").addClass("show");
-     $(".main-container").scrollTop(0);
-     $(".sidebar-link").removeClass("is-active");
-     $(".trending").addClass("is-active");
-    });
-   
-    $(".video").click(function () {
-     var source = $(this).find("source").attr("src");
-     var title = $(this).find(".video-name").text();
-     var person = $(this).find(".video-by").text();
-     var img = $(this).find(".author-img").attr("src");
-     $(".video-stream video").stop();
-     $(".video-stream source").attr("src", source);
-     $(".video-stream video").load();
-     $(".video-p-title").text(title);
-     $(".video-p-name").text(person);
-     $(".video-detail .author-img").attr("src", img);
-    });
-   });
-   
+    }
+  })
+  .resize();
 
+const allVideos = document.querySelectorAll(".video");
 
+allVideos.forEach((v) => {
+  v.addEventListener("mouseover", () => {
+    const video = v.querySelector("video");
+    video.play();
+  });
+  v.addEventListener("mouseleave", () => {
+    const video = v.querySelector("video");
+    video.pause();
+  });
+});
 
-   //jQuery time
+$(function () {
+  $(".logo, .logo-expand, .discover").on("click", function (e) {
+    $(".main-container").removeClass("show");
+    $(".main-container").scrollTop(0);
+  });
+  $(".trending, .video").on("click", function (e) {
+    $(".main-container").addClass("show");
+    $(".main-container").scrollTop(0);
+    $(".sidebar-link").removeClass("is-active");
+    $(".trending").addClass("is-active");
+  });
+
+  $(".video").click(function () {
+    var source = $(this).find("source").attr("src");
+    var title = $(this).find(".video-name").text();
+    var person = $(this).find(".video-by").text();
+    var img = $(this).find(".author-img").attr("src");
+    $(".video-stream video").stop();
+    $(".video-stream source").attr("src", source);
+    $(".video-stream video").load();
+    $(".video-p-title").text(title);
+    $(".video-p-name").text(person);
+    $(".video-detail .author-img").attr("src", img);
+  });
+});
+
+//jQuery time
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
@@ -145,8 +142,3 @@ $(".previous").click(function () {
     }
   );
 });
-
-   
-   
-   
-   
